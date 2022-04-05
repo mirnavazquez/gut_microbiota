92ccf0401b0b9237c27ab91387e728554fd0ebea
mirnavazquez <mirnavrl@mail.com>
2022-04-05 07:00:05 -0500

    Added scripts

---
+++ R/2022-04-04-IntegronFinder-Explore_output.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+# Read Integron Finder ----------------------------------------------------####
+integrons<-read_delim("0118A1023.contigs.integrons", delim = "\t",
+                      skip = 1)
+# Explore Integron Finder --------------------------------------------------####
+integrons
